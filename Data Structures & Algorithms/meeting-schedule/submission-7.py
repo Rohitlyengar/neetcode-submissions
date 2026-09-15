e2354d7aec696626bb4cafc8e6286c4e2a15ad35
Rohit Iyengar <rohitiyengar8@gmail.com>
2026-09-15 21:28:47 +0530

    Add: meeting-schedule - submission-7

---
+++ Data Structures & Algorithms/meeting-schedule/submission-7.py
@@ -0,0 +1,22 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if len(intervals) <= 1:
+            return True
+        
+        intervals.sort(key = lambda interval : interval.start)
+
+        lastEnd = intervals[0].end
+
+        for interval in intervals[1:]:
+            if interval.start < lastEnd:
+                return False
+            lastEnd = interval.end
+        return True
